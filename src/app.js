5e851e4a5623756922d4d8fa8adb064ca634a6fe
Melanie Dana Medigovich <melacem@gmail.com>
2025-04-15 15:21:13 +0000

    generdor de cartas aleatorias finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+
+  //definir la carta
+  const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const SUITS = [
+    { symbol: '♥', name: 'heart' },
+    { symbol: '♦', name: 'diamond' },
+    { symbol: '♣', name: 'club' },
+    { symbol: '♠', name: 'spade' }
+  ];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  //seleccionar elementos
+  const cardElement = document.getElementById('card');
+  const cardTop = document.querySelector('.card-top');
+  const cardCenter = document.querySelector('.card-center');
+  const cardBottom = document.querySelector('.card-bottom');
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //generador de carta aleatoria
+  const randomValue = VALUES[Math.floor(Math.random() * VALUES.length)];
+  const randomSuit = SUITS[Math.floor(Math.random() * SUITS.length)];
+
+  // aplicar clases y contenido a la carta
+  cardElement.className = 'card ' + randomSuit.name;
+  cardTop.textContent = randomSuit.symbol;
+  cardCenter.textContent = randomValue;
+  cardBottom.textContent = randomSuit.symbol;
+
+}
